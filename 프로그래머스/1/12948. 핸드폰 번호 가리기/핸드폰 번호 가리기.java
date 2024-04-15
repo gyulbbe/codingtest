0fa5475acc1d2e41a8379bd9908362420f01acab
gyulbbe <136793254+gyulbbe@users.noreply.github.com>
2024-04-16 03:09:47 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String phone_number) {
+        char[] arr = phone_number.toCharArray();
+        for(int i=0; i<arr.length-4; i++){
+            arr[i]='*';
+        }
+        String answer = String.valueOf(arr);
+        return answer;
+    }
+}
