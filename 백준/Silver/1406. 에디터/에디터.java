68de1927bd13baf26cf26f4f5caad90a1d480d92
한결 <136793254+gyulbbe@users.noreply.github.com>
2025-04-24 23:36:02 +0900

    [Silver II] Title: 에디터, Time: 408 ms, Memory: 54132 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -5,33 +5,58 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder(br.readLine());
+        char[] arr = br.readLine().toCharArray();
 		int T = Integer.parseInt(br.readLine());
-        int cursor = sb.length();
+        ArrayDeque<Character> left = new ArrayDeque<>();
+        ArrayDeque<Character> right = new ArrayDeque<>();
+        for(char c : arr) {
+            left.addLast(c);
+        }
+        
         for(int i = 0; i < T; i++) {
-            String str = br.readLine();
-            if(str.charAt(0) == 'L') {
-                if(cursor == 0 || sb.length() == 0) {
-                    continue;
-                }
-                cursor--;
-            } else if(str.charAt(0) == 'D') {
-                if(cursor == sb.length() || sb.length() == 0) {
-                    continue;
-                }
-                cursor++;
-            } else if(str.charAt(0) == 'B') {
-                if(cursor == 0 || sb.length() == 0) {
-                    continue;
-                }
-                sb.deleteCharAt(cursor - 1);
-                cursor--;
-            } else if(str.charAt(0) == 'P') {
-                sb.insert(cursor, str.charAt(2));
-                cursor++;
+            String cmd = br.readLine();
+            switch(cmd.charAt(0)) {
+                case 'L':
+                    if(!left.isEmpty()){
+                        // 커서 왼쪽 마지막을 커서 오른쪽 첫번째로 이동
+                        char leftLast = left.removeLast();
+                        right.addFirst(leftLast);
+                    }
+                    break;
+                case 'D':
+                    if(!right.isEmpty()) {
+                        // 커서 오른쪽 첫번째를 커서 왼쪽 마지막으로 이동
+                        char rightFirst = right.removeFirst();
+                        left.addLast(rightFirst);
+                    }
+                    break;
+                case 'B':
+                    if(!left.isEmpty()){
+                        // 커서 왼쪽 마지막 삭제
+                        left.removeLast();
+                    }
+                    break;
+                case 'P':
+                    // 커서 왼쪽에 마지막에 문자 추가
+                    left.addLast(cmd.charAt(2));
+                    break;
             }
         }
-        bw.write(sb.toString());
+        
+        char[] leftCharArr = new char[left.size()];
+        int i = 0;
+        while(!left.isEmpty()) {
+            leftCharArr[i++] = left.removeFirst();
+        }
+        char[] rightCharArr = new char[right.size()];
+        int j = 0;
+        while(!right.isEmpty()) {
+            rightCharArr[j++] = right.removeFirst();
+        }
+        String leftStr = String.valueOf(leftCharArr);
+        String rightStr = String.valueOf(rightCharArr);
+        
+        bw.write(leftStr + rightStr);
 		bw.flush();
         bw.close();
         br.close();
